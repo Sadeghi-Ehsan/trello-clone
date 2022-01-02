9002d1dcc354f73c13d3aae80971ea2b35c61b08
Ehsan Sadeghi <sadeghii.ehsan@gmail.com>
2022-01-02 12:43:03 +0330

    add routes, boards ,...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import {ROUTES} from "./constants/constants"
 import './App.css';
+import {BoardsPage} from "./routes/BoardsPage";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <Router>
+          <div className='h-full'>
+              <Switch>
+                  <Redirect exact from='/' to='/boards' />
+                  <Route exact path={ROUTES.BOARDS} component={BoardsPage} />
+              </Switch>
+          </div>
+      </Router>
   );
 }
 
